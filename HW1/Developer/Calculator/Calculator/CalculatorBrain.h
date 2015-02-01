c3fae4c51793a3d185c395e1b42da2ea30266376
mfscheidler <mattscheidler@outlook.com>
2015-02-01 10:10:08 -0700

    HW1
    HW1 complete

---
+++ HW1/Developer/Calculator/Calculator/CalculatorBrain.h
@@ -0,0 +1,16 @@
+//
+//  CalculatorBrain.h
+//  Calculator
+//
+//  Created by MFS on 1/30/15.
+//  Copyright (c) 2015 MFS. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CalculatorBrain : NSObject
+
+- (void)pushOperand:(double)operand;
+- (double)performOperation:(NSString *)operation;
+
+@end
